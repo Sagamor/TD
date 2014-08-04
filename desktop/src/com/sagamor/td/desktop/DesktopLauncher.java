5c541a994536a0785547c69bd419d42164664305
Alexander Sukhotin <a.sukhotin@gmail.com>
2014-08-04 15:24:05 +0400

    Baseline

---
+++ desktop/src/com/sagamor/td/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sagamor.td.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sagamor.td.TowerDefense;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TowerDefense(), config);
+	}
+}
