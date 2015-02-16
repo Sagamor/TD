c56edc38e060ff37d6f01a7d250d8eb95b287038
Alexander Sukhotin <Sagamor@Sagamor.local>
2015-02-16 23:11:08 +0100

    wip

---
+++ core/src/game/GameSettings.java
@@ -0,0 +1,15 @@
+package game;
+
+import TUIO.TuioObject;
+
+/**
+ * Created by Sagamor on 01/02/15.
+ */
+public class GameSettings {
+    public TuioObject tobj;
+    public int waveCount;
+
+    public GameSettings(TuioObject tobj) {
+        this.tobj = tobj;
+    }
+}
---
+++ core/src/game/PuzzleTask.java
@@ -0,0 +1,8 @@
+package game;
+
+/**
+ * Created by Sagamor on 01/02/15.
+ */
+public class PuzzleTask {
+
+}
